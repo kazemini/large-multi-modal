6d2716ac771946ced8ffdae105f787ce7d78d969
Ali Kazemini <kazemini.business@gmail.com>
2025-06-30 18:42:16 +0330

    fix DATASET_ROOT

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ import os
 
 class Constants:
     ## PATHs ##
-    DATASET_ROOT = '../ip102_v1.1/'
+    DATASET_ROOT = './ip102_v1.1/'
     IMG_ROOT = os.path.join(DATASET_ROOT, "images")
     TRAIN_PATH = os.path.join(DATASET_ROOT, "train.txt")
     VAL_PATH = os.path.join(DATASET_ROOT, "val.txt")
