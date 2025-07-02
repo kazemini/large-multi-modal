c70cdc51cc7a72222b4492eb3fc590e56f019079
Ali Kazemini <kazemini.business@gmail.com>
2025-07-02 15:38:25 +0330

    update constants: add gpt,gemeni, batch size and shuffle

--- constants.py
+++ constants.py
@@ -7,4 +7,9 @@ class Constants:
     TRAIN_PATH = os.path.join(DATASET_ROOT, "train.txt")
     VAL_PATH = os.path.join(DATASET_ROOT, "val.txt")
     TEST_PATH = os.path.join(DATASET_ROOT, "test.txt")
-    ID_LABEL_PATH = os.path.join('./', "classes.txt")
+    ID_CLASSES_PATH = os.path.join('./', "classes.txt")
+    PROMPT_GEMINI = os.path.join('./', "prompt_gemini.txt")
+    PROMPT_GPT = os.path.join('./', "prompt_gpt.txt")
+    PROMPT_MOAH = os.path.join('./', "simple prompt.txt")
+    BATCH_SIZE = 16
+    SHUFFLE = True
